89876608d3d3c8f4c84cc1ff0eb44b756824b54c
MKBA13 <144269975+MKBA13@users.noreply.github.com>
2024-12-17 10:56:38 +0300

    setup-test update

--- setup-tests.ts
+++ setup-tests.ts
@@ -1,5 +1,13 @@
-import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import 'jest-canvas-mock'
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import 'jest-canvas-mock';
+import { TextEncoder as NodeTextEncoder, TextDecoder as NodeTextDecoder } from 'util';
 
-configure({ adapter: new Adapter() })
+// Убедитесь, что TextEncoder и TextDecoder определены
+if (typeof global.TextEncoder === 'undefined' || typeof global.TextDecoder === 'undefined') {
+  global.TextEncoder = NodeTextEncoder as typeof TextEncoder;
+  global.TextDecoder = NodeTextDecoder as typeof TextDecoder;
+}
+
+// Настройка Enzyme
+configure({ adapter: new Adapter() });
